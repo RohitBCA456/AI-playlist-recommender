9d176b356882b458a8cbda4971bb6f74e3760b28
RohitBCA456 <ry2281104@gmail.com>
2025-04-04 18:03:02 +0530

    fix: update CORS settings in app.js and streamline dotenv configuration in index.js

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ import userRouter from "./routes/user.route.js";
 import cors from "cors";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://127.0.0.1:5500",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors({
+  origin: "https://yourmoodplaylist.netlify.app/", // Your frontend URL
+  credentials: true, // Allow cookies to be sent with requests
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Accept", "Authorization"], // Allowed headers
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/user", userRouter);
