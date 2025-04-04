1897f4ef7f660f3de9e3d06f2cb996039af58768
RohitBCA456 <ry2281104@gmail.com>
2025-04-04 21:00:38 +0530

    fix: remove trailing slash from CORS origin URL

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-  origin: "https://yourmoodplaylist.netlify.app/", // Your frontend URL
-  credentials: true, // Allow cookies to be sent with requests
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Accept", "Authorization"], // Allowed headers
+  origin: "https://yourmoodplaylist.netlify.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Accept", "Authorization"],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
