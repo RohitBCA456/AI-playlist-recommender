2ebeedf175e16200ea208e5fc9d5f51806b02e85
RohitBCA456 <ry2281104@gmail.com>
2025-04-04 20:25:45 +0530

    fix: update user API route to simplify path

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/v1/user", userRouter);
+app.use("/user", userRouter);
 
 export { app };
